a7069ede91f496b7eafc21e95829e0008f3a57ae
KJH <wnsgh9978@naver.com>
2021-09-14 13:19:19 +0900

    브랜치 병합용 커밋

--- src/kjh_test/Test.java
+++ src/kjh_test/Test.java
@@ -1,5 +1,28 @@
 package kjh_test;
 
-public class Test {
-	// 브랜치 생성용 코드
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Test extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		Image roses = new Image(getClass().getResourceAsStream("test.png"));
+		ImageView iv = new ImageView(roses);
+		FlowPane fp = new FlowPane();
+		fp.getChildren().add(iv);
+		Scene scene = new Scene(fp);
+		arg0.setScene(scene);
+		arg0.show();
+	}
 }
